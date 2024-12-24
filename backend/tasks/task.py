2c7809051ee506bb2af528ec307f72d6769e0df1
Daniel Abayomi <dan234bayomi@gmail.com>
2024-12-24 13:19:06 +0100

    quick notes on task resource

--- backend/tasks/task.py
+++ backend/tasks/task.py
@@ -7,9 +7,12 @@ from models.task import Task
 
 class TaskResource(Resource):
     # @auth_required()
+    # TODO: serialize data helper function
     def get(self, task_id=None):
         if task_id:
             task = Task.query.filter(Task.id == task_id).first()
+            if not task:
+                return jsonify({'message': 'Task not found'}), 404
             return jsonify(task)
         tasks = Task.query.all()
         return jsonify(tasks)
@@ -20,14 +23,15 @@ class TaskResource(Resource):
         if data['description']:
             new_task.deadline = data['description']
         if data['deadline']:
-            new_task.deadline = data['deadline']
+            new_task.deadline = data['deadline']  # make datetime format same
 
         db.session.add(new_task)
         db.session.commit()
 
-        return jsonify(new_task)
+        return jsonify(new_task), 201
 
     def put(self, task_id):
+        # TODO: check if id exists in db
         pass
 
     def delete(self, task_id):
