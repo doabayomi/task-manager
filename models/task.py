d04fdc275cd0778b1a42c2fbcea863de51941b49
Daniel Abayomi <dan234bayomi@gmail.com>
2024-12-31 01:24:34 +0100

    Added status field to task db model

--- models/task.py
+++ models/task.py
@@ -6,6 +6,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text)
+    status = db.Column(db.String(100), nullable=False, default='todo')
     deadline = db.Column(db.DateTime, nullable=True)
     date_added = db.Column(db.DateTime(timezone=True),
                            default=datetime.utcnow(), nullable=False)
