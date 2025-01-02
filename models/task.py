4d1fe85313d140eab9bac5525357be723ddd28aa
Daniel Abayomi <dan234bayomi@gmail.com>
2025-01-02 22:05:25 +0100

    Created basic tasks functionality for GET, POST, DELETE

--- models/task.py
+++ models/task.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.Text)
+    description = db.Column(db.Text, nullable=True)
     # status = db.Column(db.String(100), nullable=False, default='todo')
     deadline = db.Column(db.DateTime, nullable=True)
     date_added = db.Column(db.DateTime(timezone=True),
--- resources/task.py
+++ resources/task.py
@@ -1,37 +1,49 @@
 from flask import jsonify, request
 from flask_security import auth_required
 from flask_restful import Resource
+
 from models import db
 from models.task import Task
 
+from marshmallow import ValidationError
+from schemas import TaskSchema
+
 
 class TaskResource(Resource):
-    # @auth_required()
-    # TODO: serialize data helper function
+    # TODO: add authentication decorator to resource.
     def get(self, task_id=None):
         if task_id:
             task = Task.query.filter(Task.id == task_id).first()
             if not task:
-                return jsonify({'message': 'Task not found'}), 404
-            return jsonify(task)
+                return jsonify({'message': 'Task not found'}), 204
+            schema = TaskSchema()
+            return jsonify(schema.dump(task))
+
         tasks = Task.query.all()
-        return jsonify(tasks)
+        schema = TaskSchema(many=True)
+        return jsonify(schema.dump(tasks))
 
     def post(self):
-        data = request.get_json()
-        new_task = Task(name=data['name'])
-        if data['description']:
-            new_task.deadline = data['description']
-        if data['deadline']:
-            new_task.deadline = data['deadline']  # make datetime format same
+        if request.is_json:
+            data = request.get_json()
+        else:
+            data = request.form.to_dict()
+
+        try:
+            schema = TaskSchema()
+            data = schema.load(data)
+            new_task = Task(**data)
+        except ValidationError as e:
+            return {'errors': e.messages}, 400
 
         db.session.add(new_task)
         db.session.commit()
 
-        return jsonify(new_task), 201
+        return schema.dump(new_task), 201
 
     def put(self, task_id):
-        # TODO: check if id exists in db
+        # ? Do I even need a put function, no one should be
+        # ? overwriting a task completely.
         pass
 
     def delete(self, task_id):
