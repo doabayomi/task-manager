5a68fb984003732ef3327490c48e18095f14f399
Daniel Abayomi <dan234bayomi@gmail.com>
2025-01-03 01:00:31 +0100

    Added relevant comments to help in development of code

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,3 +1,4 @@
+"""Object schemas for validation, serializing and deserializing"""
 from .task_schema import TaskSchema
 
 __all__ = ['TaskSchema']
--- user/__init__.py
+++ user/__init__.py
@@ -3,4 +3,5 @@ from flask import Blueprint
 
 auth_blueprint = Blueprint('auth', __name__)
 
+# Keep other imports below this line to prevent circular imports
 from . import routes  # noqa: E402
