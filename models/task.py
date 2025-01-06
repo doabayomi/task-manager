ceeac38a3e6d2275b0b9efb1ade4c28f37070f83
Daniel Abayomi <dan234bayomi@gmail.com>
2025-01-06 15:36:09 +0100

    Changed user and task db model to link relationship between the two

--- models/task.py
+++ models/task.py
@@ -7,6 +7,11 @@ class Task(db.Model):
     name = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text, nullable=True)
     # status = db.Column(db.String(100), nullable=False, default='todo')
-    deadline = db.Column(db.DateTime, nullable=True)
+
+    deadline = db.Column(db.DateTime(timezone=True), nullable=True)
     date_added = db.Column(db.DateTime(timezone=True),
                            default=datetime.utcnow(), nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # ?: do I need a relationship back to the user
+    user = db.relationship('User', back_populates='tasks')
