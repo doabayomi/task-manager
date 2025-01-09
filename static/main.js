4a56f9a2de68a46b8620d02d25ea5115bd218ecf
Daniel Abayomi <dan234bayomi@gmail.com>
2025-01-09 10:12:50 +0100

    Set up system for dynamic imports form riot components to specific html templates for flask app

--- static/main.js
+++ static/main.js
@@ -1,10 +1,25 @@
 import './css/main.css';
 import * as riot from 'riot';
-import App from './app.riot';
 
-const mountApp = riot.component(App);
+document.addEventListener('DOMContentLoaded', () => {
+  const page = document.body.getAttribute('data-page');
 
-const app = mountApp(
-  document.getElementById('root'),
-  { message: 'Hello World', items: [] }
-);
+  switch (page) {
+    case 'home':
+      import('@components/pages/home.riot').then((module) => {
+        // Mount the component to the '#app' div
+        riot.mount('#app', module.default); // Ensure module.default is usec.
+      });
+      break;
+    
+    case 'login':
+      import('@components/pages/login-form.riot').then((module) => {
+        riot.mount('#app', module.default)
+      })
+      riot.mount('#app', Home);
+    
+    // As many pages as needed we can just export the components to it.
+    default:
+      break;
+  }
+})
