87d98ae230cb4ef4461740b0496436edfb91c4c8
Daniel Abayomi <dan234bayomi@gmail.com>
2025-01-09 13:56:05 +0100

    Added css files, modified main.js for dynamic imports and organized riot files with alias for compilation in webpack

--- static/main.js
+++ static/main.js
@@ -1,25 +1,29 @@
 import './css/main.css';
 import * as riot from 'riot';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const page = document.body.getAttribute('data-page');
+const page = document.body.getAttribute('data-page');
 
-  switch (page) {
-    case 'home':
-      import('@components/pages/home.riot').then((module) => {
-        // Mount the component to the '#app' div
-        riot.mount('#app', module.default); // Ensure module.default is usec.
-      });
-      break;
-    
-    case 'login':
-      import('@components/pages/login-form.riot').then((module) => {
-        riot.mount('#app', module.default)
-      })
-      riot.mount('#app', Home);
-    
-    // As many pages as needed we can just export the components to it.
-    default:
-      break;
-  }
-})
+switch (page) {
+  
+  case 'home':
+    import('@components/home.riot').then((module) => {
+      riot.register('app', module.default);
+      riot.mount('app'); // Mount the component
+    }).catch((error) => {
+      console.error('Error loading home component:', error);
+    });
+    break;
+  
+  case 'login':
+    import('@components/authentication/login-form.riot').then((module) => {
+      riot.register('app', module.default);
+      riot.mount('app');
+    }).catch((error) => {
+      console.error('Error loading page:', error)
+    })
+    break;
+  
+  // As many pages as needed we can just export the components to it.
+  default:
+    break;
+}
